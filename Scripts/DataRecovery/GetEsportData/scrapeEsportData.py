65fb02b30668d18039fab383061c217cfff1a698
Eliott Frohly <frohlyeliott@gmail.Com>
2024-01-23 16:35:50 -0500

    Headers added to scraping

--- Scripts/DataRecovery/GetEsportData/scrapeEsportData.py
+++ Scripts/DataRecovery/GetEsportData/scrapeEsportData.py
@@ -5,13 +5,21 @@
 import requests
 from lxml import html
 
+
+headers = {
+    'authority': 'www.google.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+}
 url = "https://gol.gg/tournament/tournament-matchlist/LFL%20Spring%202024/"
 match_urls = {}
 match_data = {}
 i = 1
 
 # Get the content of the div with the specified XPath
-response = requests.get(url)
+response = requests.get(url,headers=headers)
 tree = html.fromstring(response.content)
 
 while True:
@@ -26,14 +34,10 @@ while True:
     i += 1
 
 for match,match_url in match_urls.items():
-    response = requests.get(match_url)
-    print(response.text)
+    response = requests.get(match_url,headers=headers)
     tree = html.fromstring(response.content)
     match_data[match] = {}
     GameTime_XPath = "/html/body/div"
-    print(tree.xpath(GameTime_XPath))
-    print(match)
-    print(match_url)
     GameTime = tree.xpath(GameTime_XPath)[0].text
     match_data[match]["GameTime"] = GameTime
 
