2b05dc596dcdb699605a218d223324be727b8d9a
Clément Tourte <yanquille92@gmail.com>
2024-01-24 22:27:45 +0100

    Changes to scrapegolclem.py

--- Scripts/DataRecovery/GetEsportData/scrapeGoLClem.py
+++ Scripts/DataRecovery/GetEsportData/scrapeGoLClem.py
@@ -9,23 +9,15 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
 }
 
-LEC_WINTER_URL = "https://gol.gg/tournament/tournament-matchlist/LEC%20Winter%20Season%202024/"
+URL = "https://gol.gg/tournament/tournament-matchlist/LEC%20Winter%20Season%202024/"
 site_url = "https://gol.gg/"
-response = requests.get(LEC_WINTER_URL, headers = headers)
+response = requests.get(URL, headers = headers)
 html_content = response.content
+
 
 soup = BeautifulSoup(html_content, "html.parser")
 
 game_links = soup.find_all('a', title=lambda value : value and 'vs' in value)
-team_names = set()
-team_names_dict = {}
-td_elements = soup.find_all('td',class_=['text-right','text-left'])
-
-for td_element in td_elements:
-    team_name = td_element.text.strip()
-
-    if team_name:
-        team_names_dict[team_name] = {}
 
 for link in game_links:
     match_link = site_url + link.get('href')[3:]
@@ -37,15 +29,9 @@ for link in game_links:
 
     h1_elements = match_soup.find_all('h1')
 
-
-for i in range(len(h1_elements)):
-    gametime_bool = ":" in h1_elements[i].text.strip()
-    if gametime_bool:
-        print("matchtime : " + h1_elements[i].text.strip())
-    else:
-        print("team matchup : " + h1_elements[i].text.strip())
-
-for team_name, data in team_names_dict.items():
-    print("Team Name:", team_name)
-    print("Team Matchup:", data.get('team_matchup', 'N/A'))
-    print("Matchtime:", data.get('matchtime', 'N/A'))
+    for i in range(len(h1_elements)):
+        gametime_bool = ":" in h1_elements[i].text.strip()
+        if gametime_bool:
+           print("matchtime : " + h1_elements[i].text.strip())
+        else:
+            print("team matchup : " + h1_elements[i].text.strip())
